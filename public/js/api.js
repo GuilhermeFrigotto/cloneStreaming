225b5423a9dfd1a45358b89c862a4d020daeae52
guilhermefrigotto <guilherme.frigotto@gmail.com>
2023-01-05 11:16:01 -0400

    copy Netflix

---
+++ public/js/api.js
@@ -0,0 +1,8 @@
+let api_key = "6710df2c80f5b394b488197cd6aad672";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
+
